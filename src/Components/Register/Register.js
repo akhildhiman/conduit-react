f0e12d418e61bd23fda3d2a63810536aef537bc4
akhil dhiman <dhimanakhil95@gmail.com>
2019-10-23 15:56:58 +0530

    add signup feature

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,32 +1,90 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "react-router-dom";
 import "./Register.css"
 
 
 
-function Register() {
-    function handleClick() {
+class Register extends Component {
+    constructor() {
+        super()
+        this.state = {
+            userName: "",
+            email: "",
+            password: ""
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault(); 
+
+        const body = {
+            user:{
+                username:this.state.userName,
+                email:this.state.email,
+                password: this.state.password
+            }
+        }
+
         fetch("https://conduit.productionready.io/api/users", {
             method: 'POST', // or 'PUT'
-            // body: JSON.stringify(data), // data can be `string` or {object}!e
+            body: JSON.stringify(body), // data can be `string` or {object}!e
             headers: {
               'Content-Type': 'application/json'
             }
           })
-        .then(response => response.json())
+        .then(response => response.json()).then(data=>console.log(data))
+
     }
 
-    return(
-        <form className="form">
-            <input type="text" placeholder="Enter the username" /> <br></br>
-            <input type="text" placeholder="Enter the email" /><br></br>
-            <input type="text" placeholder="Enter the password" /><br></br>
-            <button type="submit">Signup</button><br></br>
-            <Link to="/login">
-                <button onClick={handleClick}type="submit">Already an user? Login</button>
-            </Link>
-        </form>
-    )
-}
+    handleChange = (e) => {
+        const { name,value } = e.target
+        this.setState({
+            [name] : value //computed value of whatever it is in the name attribute
+        })
+    }
+
+
+    //     fetch("https://conduit.productionready.io/api/users", {
+    //         method: 'POST', // or 'PUT'
+    //         // body: JSON.stringify(data), // data can be `string` or {object}!e
+    //         headers: {
+    //           'Content-Type': 'application/json'
+    //         }
+    //       })
+    //     .then(response => response.json())
+    // }
+
+
+    render() {
+        return(
+            <form className="form">
+
+                <input type="text"
+                name="userName" 
+                placeholder="Enter the username" 
+                value={this.state.userName} 
+                onChange={this.handleChange}/>
+                 <br></br>
+
+                <input type="text"
+                 placeholder="Enter the email" 
+                 value={this.state.email} 
+                 name="email" 
+                 onChange={this.handleChange}/>
+                 <br></br>
+
+                <input type="text"
+                 placeholder="Enter the password" 
+                 value={this.state.password} 
+                 name="password"
+                 onChange={this.handleChange}/>
+                 <br></br>
+
+                <button type="submit" onClick={this.handleSubmit}>Signup</button><br></br>
+
+                <Link to="/login">  Already an user? Login  </Link>
+            </form>
+        )
+}}
 
 export default Register;
